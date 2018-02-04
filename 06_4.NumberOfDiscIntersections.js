a2b22927b93a433fda3401cf06205606951e08c7
A S M Sadiqul Islam <sadikultra@gmail.com>
2018-02-04 21:20:11 +0600

    Solved 100%: 6.4 NumberOfDiscIntersections

--- 06_4.NumberOfDiscIntersections.js
+++ 06_4.NumberOfDiscIntersections.js
@@ -1,36 +1,68 @@
 /**
   * 6.4 NumberOfDiscIntersections
   * Compute the number of intersections in a sequence of discs.
-  * result: 87%, https://app.codility.com/demo/results/trainingJXX6XY-4QF/
+  * result: 100%, https://app.codility.com/demo/results/trainingSYV7GD-X3N/
   */
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
+    let result = 0
+    let starts = []
+    let ends = []
+    let length = A.length
     
-    let count = 0
-    
-    let circles = []
-    
-    A.map((a, i) => {
-        circles.push([i-a, i+a])
+     A.map((a, i) => {
+        starts.push(i-a)
+        ends.push(i+a)
     })
+    starts.sort((a,b) => a-b)
+    ends.sort((a,b) => a-b)
     
-    circles.sort((a,b) => a[0] - b[0])
+    // console.log(starts, ends)
+    let count = 0
+    let a = 0
+    let b = 0
     
-    // console.log(circles)
-    
-    for(let i = 1; i < circles.length; i++){
-        for(let j = 0; j < i; j++)
-            if(circles[j][1] - circles[i][0] >= 0){
-                if(++count > 10000000){
-                    count = -1
-                    break
-                }
-            }
-        if(count == -1)
-            break
+    while(a < length || b < length){
+        if (result > 10000000)
+            return -1
+        
+        if(a < length && starts[a] <= ends[b]){
+            count++
+            a++
+            // console.log('+', count, result, a, b, starts[a], ends[b])
+        }else{
+            count--
+            result += count
+            b < length ? b++ : 0
+            // console.log('-', count, result, a, b, starts[a], ends[b])
+        }
     }
     
-    
-    return count
+    return result
 }
+
+/*
+
+-4 1
+-1 4
+ 0 4
+ 0 5
+ 2 6
+ 5 8
+ --------
+  0 0 0 0 
++ 1 0 1 0 -4 1
++ 2 0 2 0 -1 1
++ 3 0 3 0  0 1
++ 4 0 4 0  0 1
+- 3 3 4 1  2 1
++ 4 3 5 1  2 4
+- 3 6 5 2  5 4
+- 2 8 5 3  5 4
++ 3 8 6 3  5 6
+- 2 10 6 4 u 8 
+- 1 11 6 5 u u
+- 0 11 6 6 
+
+*/
