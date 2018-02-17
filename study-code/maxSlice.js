545ce5a5ffb9e61be7107291921ba481856f1499
A S M Sadiqul Islam <sadikultra@gmail.com>
2018-02-18 02:12:45 +0600

    Update maxSlice.js

--- study-code/maxSlice.js
+++ study-code/maxSlice.js
@@ -1,3 +1,5 @@
+// !DOES NOT WORK ON SUM < 0
+// See this code: https://github.com/SadiqUltra/codility/blob/master/09_1.MaxSliceSum.js 
 const maxSlice = (A) => {
 	let maxEnding = 0
 	let maxSlice = 0
