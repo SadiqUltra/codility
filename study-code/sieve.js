7de6486c6c0269726e144f6b74b57eae82ec560e
A S M Sadiqul Islam <sadikultra@gmail.com>
2018-04-11 16:08:05 +0600

    Create sieve.js

---
+++ study-code/sieve.js
@@ -0,0 +1,14 @@
+function sieve(n) {
+    let sieve = Array.from(Array(n), () => true)
+    sieve[0] = sieve[1] = false
+    
+    for(let i = 2; i*i < n; i++){
+        if(sieve[i]){
+            for(let j = i*i; j < n; j += i){
+                sieve[j] = false
+            }
+        }
+    }
+    
+    return sieve
+}
